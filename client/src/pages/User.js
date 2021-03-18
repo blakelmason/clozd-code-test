50a6152f9ce7f15206398937d7cc56d90fa2debe
Blake <blakelmason@gmail.com>
2021-03-17 23:06:40 -0600

    user styling

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -7,27 +7,31 @@ export default function User({ user }) {
       <div className="text-center mb-5">
         <Link to="/">Home</Link>
       </div>
-      <div className="d-lg-flex align-items-center justify-content-center">
-        <img
-          className="border rounded shadow-sm me-lg-3 mb-3 mb-lg-0"
-          src={user.picture.large}
-          alt=""
-        />
-        <p className="mb-0">
-          <strong>Name:</strong> {user.name.first} {user.name.last}
-          <br />
-          <strong>Email:</strong> {user.email}
-          <br />
-          <strong>
-            <u>Address</u>
-          </strong>
-          <br />
-          {user.location.street.number} {user.location.street.name}
-          <br />
-          {user.location.city}, {user.location.state} {user.location.postcode}
-          <br />
-          {user.location.country}
-        </p>
+      <div className="d-flex justify-content-center">
+        <div className="d-lg-flex align-items-center justify-content-center border rounded shadow p-4">
+          <div className="text-center text-lg-start">
+            <img
+              className="border rounded shadow-sm me-lg-3 mb-3 mb-lg-0"
+              src={user.picture.large}
+              alt=""
+            />
+          </div>
+          <p className="mb-0">
+            <strong>Name:</strong> {user.name.first} {user.name.last}
+            <br />
+            <strong>Email:</strong> {user.email}
+            <br />
+            <strong>
+              <u>Address</u>
+            </strong>
+            <br />
+            {user.location.street.number} {user.location.street.name}
+            <br />
+            {user.location.city}, {user.location.state} {user.location.postcode}
+            <br />
+            {user.location.country}
+          </p>
+        </div>
       </div>
     </div>
   )
