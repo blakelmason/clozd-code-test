9d61e990e19b9d0c9d750b52301ea52cbac29d14
Blake <blakelmason@gmail.com>
2021-03-17 22:13:48 -0600

    heroku react-router problem

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ async function startServer() {
   })
 
   server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname + '/../build/index.html'))
   })
 
   server.listen(PORT, () => {
